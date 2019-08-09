c9f9ffe5ab4cf8c6057db26e887fdc187f2d0eed
xellux <41974585+xellux@users.noreply.github.com>
2019-08-09 18:59:31 -0500

    Complete DB

---
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [MaxLength(20, ErrorMessage = "The {0} field cannot have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Document { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+
+        public string Address { get; set; }
+
+        [Display(Name = "Owner's Name")]
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+
+        public ICollection<Property> Properties { get; set; }
+
+        public ICollection<Contract> Contracts { get; set; }
+    }
+}
