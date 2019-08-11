109e16b5d06e38b91bfaa2a20143785328c0dec7
xellux <41974585+xellux@users.noreply.github.com>
2019-08-11 18:11:02 -0500

    User Identities

--- MyLeasing.Web/Data/DataContext.cs
+++ MyLeasing.Web/Data/DataContext.cs
@@ -1,15 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MyLeasing.Web.Data.Entities;
 
 namespace MyLeasing.Web.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
         public DbSet<Contract> Contracts { get; set; }
         public DbSet<Lessee> Lessees { get; set; }
+        public DbSet<Manager> Managers { get; set; }
         public DbSet<Owner> Owners { get; set; }
         public DbSet<Property> Properties { get; set; }
         public DbSet<PropertyImage> PropertyImages { get; set; }
--- MyLeasing.Web/Data/Entities/Owner.cs
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -1,42 +1,11 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace MyLeasing.Web.Data.Entities
 {
     public class Owner
     {
         public int Id { get; set; }
-
-        [MaxLength(20, ErrorMessage = "The {0} field cannot have more than {1} characters.")]
-        [Required(ErrorMessage = "The field {0} is mandatory.")]
-        public string Document { get; set; }
-
-        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        [Required(ErrorMessage = "The field {0} is mandatory.")]
-        [Display(Name = "First Name")]
-        public string FirstName { get; set; }
-
-        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        [Required(ErrorMessage = "The field {0} is mandatory.")]
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-
-        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        [Required(ErrorMessage = "The field {0} is mandatory.")]
-        [Display(Name = "Fixed Phone")]
-        public string FixedPhone { get; set; }
-
-        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        [Required(ErrorMessage = "The field {0} is mandatory.")]
-        [Display(Name = "Cell Phone")]
-        public string CellPhone { get; set; }
-
-        public string Address { get; set; }
-
-        [Display(Name = "Owner's Name")]
-        public string FullName => $"{FirstName} {LastName}";
-
-        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+        public User User { get; set; }
 
         public ICollection<Property> Properties { get; set; }
 
