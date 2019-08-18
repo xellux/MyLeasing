3f6a6deb38152c6f8370e1b72b9e8f08c0c9da0b
xellux <41974585+xellux@users.noreply.github.com>
2019-08-18 18:02:50 -0500

    Account Controller and Login logout

--- MyLeasing.Web/Helpers/UserHelper.cs
+++ MyLeasing.Web/Helpers/UserHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using MyLeasing.Web.Data.Entities;
+using MyLeasing.Web.Models;
 using System.Threading.Tasks;
 
 namespace MyLeasing.Web.Helpers
@@ -8,13 +9,16 @@ namespace MyLeasing.Web.Helpers
     {
         private readonly UserManager<User> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<User> _signInManager;
 
         public UserHelper(
             UserManager<User> userManager,
-            RoleManager<IdentityRole> roleManager)
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<User> signInManager)
         {
             _userManager = userManager;
             _roleManager = roleManager;
+            _signInManager = signInManager;
         }
 
         public async Task<IdentityResult> AddUserAsync(User user, string password)
@@ -48,6 +52,21 @@ namespace MyLeasing.Web.Helpers
         {
             return await _userManager.IsInRoleAsync(user, roleName);
         }
+
+        public async Task<SignInResult> LoginAsync(LoginViewModel model)
+        {
+            return await _signInManager.PasswordSignInAsync(
+                model.Username,
+                model.Password,
+                model.RememberMe,
+                false); //Failed attempts Lockout Account 
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+        }
+
     }
 }
 
