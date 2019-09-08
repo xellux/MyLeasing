54a37c61c4a59c8cb16cb391fdb091676b66198d
xellux <41974585+xellux@users.noreply.github.com>
2019-09-08 17:02:58 -0500

    Complex API

---
+++ MyLeasing.Common/Models/EmailRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+
+namespace MyLeasing.Common.Models
+{
+    public class EmailRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
